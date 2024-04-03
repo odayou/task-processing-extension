d9bf11e18bc8502995da6f7e8ac028318c228fdd
tomy.li <tomy.li@corp.to8to.com>
2024-04-04 01:18:21 +0800

    add feature:copy markdown link info

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Editor, Plugin } from 'obsidian';
+import { Editor, Plugin, Notice, Menu, MenuItem } from 'obsidian';
 
 interface TimeSaverPluginSettings {
 	autoCompute: string;
@@ -10,7 +10,18 @@ const DEFAULT_SETTINGS: TimeSaverPluginSettings = {
 
 export default class TimeSaverPlugin extends Plugin {
 	settings: TimeSaverPluginSettings;
+
 	async onload() {
+		/**
+		 * 注册编辑器右键监听事件
+		 */
+		this.registerEvent(this.app.workspace.on("editor-menu", (menu, editor, view) => {
+			const markdownLinkFromActiveText = getMarkdownLinkFromCursorPoint(editor);
+			if (markdownLinkFromActiveText) {
+				addContextMenu( markdownLinkFromActiveText, menu, editor);
+			}
+		}));
+
 		const taskExp = /\s*(-|\*)\s+\[(\s|\w)\]\s+/g
 		this.addCommand({
 			id: "compute-total-time",
@@ -33,6 +44,14 @@ export default class TimeSaverPlugin extends Plugin {
 				})
 				notice += `\n\n---\n花费总时长:${minutesToHours(allSum)}小时\n\n最后统计时间: ${new Date().toLocaleString()}\n\n---\n`;
 				editor.replaceRange(notice, editor.getCursor());
+			}
+		});
+
+		this.addCommand({
+			id: "copy-url-and-txt",
+			name: "Copy markdown link info",
+			editorCallback: (editor: Editor) => {
+				copyMarkDownLinkTextAndInfo(editor)
 			}
 		});
 
@@ -117,4 +136,127 @@ function minutesToHours(minutes: number, toFixed = 2) {
 	// 分钟数转换为小时数，精确到小数点后两位
 	const diffInHours = (minutes / 60).toFixed(toFixed);
 	return diffInHours;
+}
+
+function isMarkdownUrl(str: String) {
+	return str.match(/!\[[^\]]*\]\(([^)]+)\)/) == null
+}
+
+function getMarkdownUrlInfo(str: String) {
+	/// 定义正则表达式
+	const regex = /\[([^\]]+)\]\(([^)]+)\)/;
+
+// 使用正则表达式匹配
+	const match = str.match(regex);
+	if (match) {
+		const text = match[1]; // 获取链接文本
+		const url = match[2]; // 获取链接 URL
+
+		return {
+			url,
+			text
+		}
+	} else {
+		return {
+			url: '',
+			text: ''
+		}
+	}
+}
+
+function isMouseHoverOnMarkdownLink(line: string, cursorPosition: number): boolean {
+	const linkRegex = /\[.*?\]\((.*?)\)/g;
+	let match;
+
+	while ((match = linkRegex.exec(line)) !== null) {
+		if (match.index <= cursorPosition && cursorPosition <= match.index + match[0].length) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * 获取当前光标下的markdown链接
+ * @param line
+ * @param cursorPosition
+ */
+function getMarkdownLink(line: string, cursorPosition: number): string | null {
+	const linkRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
+	let match;
+
+	while ((match = linkRegex.exec(line)) !== null) {
+		if (match.index <= cursorPosition && cursorPosition <= match.index + match[0].length) {
+			return match[0]; // 返回整个Markdown链接部分
+		}
+	}
+
+	return null; // 如果光标不在Markdown链接内，则返回null
+}
+
+/**
+ * Add context menu for markdown link.
+ * @param linkInfo
+ * @param menu
+ * @param editor
+ */
+function addContextMenu(linkInfo: any, menu: Menu, editor: Editor) {
+		menu.addItem((menuItem) => {
+			menuItem.setTitle("Copy markdown link info");
+			menuItem.setIcon("clipboard-copy");
+			menuItem.onClick(() => {
+				copyMarkDownLinkTextAndInfoFromText(linkInfo)
+			});
+		});
+}
+
+/**
+ * 从文本中复制markdown链接的文本和url到剪切板
+ * @param editor
+ */
+function copyMarkDownLinkTextAndInfo(editor: Editor) {
+	const markdownLinkFromActiveText= getMarkdownLinkFromCursorPoint(editor);
+
+	copyMarkDownLinkTextAndInfoFromText(markdownLinkFromActiveText)
+}
+
+/**
+ * 从文本中复制markdown链接的文本和url到剪切板
+ * @param linkInfo
+ */
+function copyMarkDownLinkTextAndInfoFromText(linkInfo: any) {
+	if (linkInfo == null) {
+		new Notice("没有markdown链接");
+		return null;
+	}
+
+	// 复制markdown链接中的文本和url到剪切板
+	let promise = navigator.clipboard.writeText(linkInfo.text + " " + linkInfo.url);
+	promise.then(
+		() => {
+			new Notice("已复制到剪切板");
+		},
+		() => {
+			new Notice("复制到剪切板失败");
+		}
+	)
+}
+
+/**
+ * 从光标位置获取markdown链接信息
+ * @param editor
+ */
+function getMarkdownLinkFromCursorPoint(editor: Editor) {
+	const cursor = editor.getCursor();
+	const line = editor.getLine(cursor.line);
+
+	const markdownLink = getMarkdownLink(line, cursor.ch);
+	if (markdownLink == null) {
+		new Notice("没有markdown链接");
+		return null;
+	}
+
+	// 复制markdown链接中的文本和url到剪切板
+	return getMarkdownUrlInfo(markdownLink);
 }
